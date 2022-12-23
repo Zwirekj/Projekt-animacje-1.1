5c04452f81a602d51a16d148a7d0367c42ccc069
Zwirekj <101943307+Zwirekj@users.noreply.github.com>
2022-12-23 16:48:10 +0100

    Add files via upload

---
+++ Projekty-strony-internetowe/Projekt-strona-internetowa/js/slicksettings.js
@@ -0,0 +1,20 @@
+$('.team-carousel').slick({
+	arrows: false,
+	autoplaySpeed: 2500,
+	autoplay: true,
+	mobileFirst: true,
+
+	slidesToShow: 1,
+	slidesToScroll: 1,
+
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: { slidesToShow: 2 },
+		},
+		{
+			breakpoint: 992,
+			settings: { slidesToShow: 3 },
+		},
+	],
+});
